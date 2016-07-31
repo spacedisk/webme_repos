15df136ef4540329eadfa0fdacc1024899e04e02
inspiron <spacedisks@gmail.com>
2016-07-31 12:53:42 +0530

    added  html templates into  site.

---
+++ webme/webme_urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from .import  views
+
+#define the url here
+
+#enroute the url request to corresponding views
+#here the mainpage is landing page 
+
+#pattern match when no text in between ^ and $ start and end 
+#in url request
+urlpatterns =[url(r'^$',views.main_page ,name = 'main_page'),]
